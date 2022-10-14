068d62a4d5e0e8b76fb5e7329cc5414ad5d8a02f
githubPrabhsingh <101601501+githubPrabhsingh@users.noreply.github.com>
2022-10-14 12:04:41 +0530

    Create sqrt_x.cpp

---
+++ sqrt_x.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+using namespace std;
+long long int sqrtinteger(int n)
+    {
+   int s=0;
+    int e =n;
+    long long int mid =s+(e-s)/2;
+       long long int ans=-1;
+     
+        while(s<=e)
+        {
+             long long int square=mid*mid;
+          if(square==n)
+            return mid;
+         if(square>n)
+            e=mid-1;
+        else
+        {
+            ans=mid;
+             s=mid+1;
+        }
+            mid =s+(e-s)/2;
+        }
+        return ans;
+        }
+double moreprecision(int n, int precision,int tempsol)
+{
+    double factor=1;
+    double ans =tempsol;
+    for(int i=0;i<precision;i++)
+    {
+        factor=factor/10;
+    for(double j=ans ; j*j<n;j=j+factor)
+    {
+        ans=j;
+    }
+    }
+    return ans;
+}
+int main()
+{
+    int n,p;
+    cout<<"Enter the number :"<<endl;
+    cin>>n;
+    cout<<"Enter the precision :"<<endl;
+    cin>>p;
+    int tempsol=sqrtinteger(n);
+    cout<<tempsol<<endl;
+    cout<<"Answer is "<<moreprecision(n,p,tempsol)<<endl;
+
+    return 0;
+}
