c1a672238270abc02607a4ec860225c38b868746
githubPrabhsingh <101601501+githubPrabhsingh@users.noreply.github.com>
2022-10-14 12:12:26 +0530

    pattern_1.cpp

---
+++ pattern_1.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int i=1,n,j;
+    cin>>n;
+    while(i<=n)//this is moving row.
+    {
+        int j=1;
+        while(j<=n) //this is moving column.
+        {
+            cout<<j; // we are printing j because we need 1234 1234 1234 1234 type pattern and for 1 row j is varying from  1 to n.
+            j++;
+        }
+        cout<<endl;
+        i++;
+    }
+return 0;
+}
